55c477cd6591515a068889fb544a0bfef516578e
ogbanakar <86116916+ogbanakar@users.noreply.github.com>
2022-06-07 15:32:58 +0530

    init

--- fish/src/App.js
+++ fish/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/navbar';
 import './App.css';
-
+import Content from './components/Content';
 function App() {
   return (
     <div >
       <Navbar />
+      <Content />
     </div>
   );
 }
---
+++ fish/src/components/Content.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./content.scss";
+
+const Content = () => {
+  return (
+    <div className="content">
+      <h1>STEP 1</h1>
+      <p>
+        Obviously the first the to do when a want keep fish is to get a tank The
+        basic starter tank is of 5 gallon
+        <br />
+        or 19 ltr tank.
+      </p>
+
+      <h1>STEP 2</h1>
+      <p>
+        The next step is not to buy fish and drop them in there Instead you have
+        to buy a filter and add water
+        <br />
+        ,Set up the filter and run the filter with the water in it for  a couple of days
+      </p>
+    </div>
+  );
+};
+
+export default Content;
