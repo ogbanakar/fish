cdc131cd6cc4cf1d8fceaa0765aa2d61fc42f05a
ogbanakar <86116916+ogbanakar@users.noreply.github.com>
2023-05-22 21:28:27 +0530

    first commit

---
+++ hawk/src/components/MenuData.jsx
@@ -0,0 +1,36 @@
+export const MenuData = [
+  {
+    title: "Home",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-house-user",
+  },
+  {
+    title: "About",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-circle-info",
+  },
+  {
+    title: "Services",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-briefcase",
+  },
+  {
+    title: "Contact",
+    url: "#",
+    cName: "nav-links",
+    icon: "fa-solid fa-address",
+  },
+  {
+    title: "Book Now!",
+    url: "#",
+    cName: "nav-links-mobile",
+    
+  },
+];
+
+
+///this compnet is a list which controls the kinks in the navbar
+/// add new links or buttons here dont mess with the navbar compnent
